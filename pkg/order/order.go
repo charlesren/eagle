a38975538f2d030d001770eb432fda9df4461c57
rencc <renccn@gmail.com>
2020-07-15 18:23:49 +0800

    init

--- pkg/order/order.go
+++ pkg/order/order.go
@@ -3,12 +3,20 @@ package order
 import "time"
 
 type Order struct {
-	OrderID         string
-	StockName       string
-	StockCode       string
-	Flag            string
-	Price           float64
-	Number          int64
-	Time            time.Time
-	ShareholderCode string
+	OrderID           string
+	StockName         string
+	StockCode         string
+	OrderType         orderType
+	TransactionPrice  float64
+	TransactionVolume int64
+	TransactionTime   time.Time
+	TransactionAmount float64
+	ShareholderCode   string
 }
+
+type orderType string
+
+const (
+	Buying  orderType = "Buying"
+	Selling orderType = "Selling"
+)
