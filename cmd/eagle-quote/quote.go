2ce13450075f3c771a1aca761a52b257d6c683dc
charlesren <renccn@gmail.com>
2020-07-25 17:23:50 +0800

    init

--- cmd/eagle-quote/quote.go
+++ cmd/eagle-quote/quote.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/charlesren/eagle/cmd/eagle-quote/app"
 )
 
 func main() {
+	stockName := os.Args[1]
+	if stockName == "" {
+		fmt.Println("stock code is nil")
+		return
+	}
+	interval, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Println("wrong interval")
+	}
 	var data app.SinaQuoteMetrics
-	data = app.Get("000027.SZ")
-	fmt.Println(data)
+	for {
+		data = app.Get(stockName)
+		fmt.Println(data)
+		time.Sleep(time.Duration(interval) * time.Second)
+	}
 }
