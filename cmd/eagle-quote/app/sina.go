4208645dfb893d965843ccb1787e832980d7f316
charlesren <renccn@gmail.com>
2020-07-25 16:12:01 +0800

    init

---
+++ cmd/eagle-quote/app/sina.go
@@ -0,0 +1,30 @@
+package app
+
+import quotesina "github.com/charlesren/sina"
+
+//SinaQuoteMetrics is realtime metrics of stock
+type SinaQuoteMetrics struct {
+	Open     float64
+	Now      float64
+	High     float64
+	Low      float64
+	Turnover int
+	Volume   float64
+	Date     string
+	Time     string
+}
+
+//Get func get lastest value
+func Get(stockCode string) SinaQuoteMetrics {
+	var sqm SinaQuoteMetrics
+	tmpData := quotesina.GetData(stcokCode)
+	sqm.Open = tmpData.Open
+	sqm.Now = tmpData.Now
+	sqm.High = tmpData.High
+	sqm.Low = tmpData.Low
+	sqm.Turnover = tmpData.Turnover
+	sqm.Volume = tmpData.Volume
+	sqm.Date = tmpData.Date
+	sqm.Time = tmpData.Time
+	return sqm
+}
