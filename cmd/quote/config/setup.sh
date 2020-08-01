2ee93d752abaa6f42d994f8ce37ed7d8422f814c
charlesren <renccn@gmail.com>
2020-08-01 22:13:51 +0800

    init

--- cmd/quote/config/setup.sh
+++ cmd/quote/config/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 git clone https://github.com/charlesren/eagle.git
-cd eagle/cmd/eagle-quote/build
+cd eagle/cmd/quote/build
 GOOS=linux go build ../quote.go
 docker build -t quote:v1 .
 rm -f quote
