c407ad59aedea793466b075dd030a06bfcbc90c8
charlesren <renccn@gmail.com>
2021-03-22 15:33:13 +0800

    change order define

--- pkg/order/order.go
+++ pkg/order/order.go
@@ -3,20 +3,22 @@ package order
 import "time"
 
 type Order struct {
-	OrderID           string
-	StockName         string
-	StockCode         string
-	OrderType         orderType
-	TransactionPrice  float64
-	TransactionVolume int64
-	TransactionTime   time.Time
-	TransactionAmount float64
-	ShareholderCode   string
+	StockCode           string
+	StockName           string
+	OrderType           orderType
+	TransactionDate     time.Time
+	TransactionDateTime time.Time
+	TransactionPrice    float64
+	TransactionVolume   int64
+	TransactionAmount   float64
+	TransactionID       string
+	OrderID             string
+	ShareholderCode     string
 }
 
 type orderType string
 
 const (
-	Buying  orderType = "Buying"
-	Selling orderType = "Selling"
+	Buy  orderType = "Buy"
+	Sell orderType = "Sell"
 )
